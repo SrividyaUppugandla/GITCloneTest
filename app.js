b31ce46fdd5108342c174edf151f337999dcc69c
SrividyaUppugandla <srividya.uppugandla@cognizant.com>
2016-06-14 18:01:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use(function (req, res) {
 
 // START THE SERVER
 app.listen(port);
-console.log("App listening on port " + port + 'current working dir ' + process.cwd() + 'Environment:' + JSON.stringify(process.env));
+console.log("App listening on port " + port + 'current working dir ' + process.cwd() + 'Environment:' + process.env.linkedin);
 
 module.exports = app;
